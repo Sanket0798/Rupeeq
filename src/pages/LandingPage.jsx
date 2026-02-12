35b30bde14693b18d7d0cb1ab5c98fd3e440bcfa
sanket0798 <sanketrathod1398@gmail.com>
2026-02-12 13:15:56 +0530

    Minor Changes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,41 +1,13 @@
 // Import sections directly from sections folder
 import {
-  Hero,
-  Milestones,
   HowItWorks,
-  CreditScore,
-  Products,
-  Partners,
-  Testimonials,
-  FAQ
 } from '../components/sections';
 
 const LandingPage = () => {
   return (
     <>
-      <section id="hero">
-        <Hero />
-      </section>
-      <section id="milestones">
-        <Milestones />
-      </section>
       <section id="how-it-works">
         <HowItWorks />
-      </section>
-      <section id="credit-score">
-        <CreditScore />
-      </section>
-      <section id="products">
-        <Products />
-      </section>
-      <section id="partners">
-        <Partners />
-      </section>
-      <section id="testimonials">
-        <Testimonials />
-      </section>
-      <section id="faq">
-        <FAQ />
       </section>
     </>
   );
